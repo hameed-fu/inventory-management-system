812fbae7b2867bd2444463a604733505dc713eda
mustafaviit <mustafakamalawkumcs@gmail.com>
2023-03-08 04:50:22 -0800

    create models

--- resources/views/backend/sales/index.blade.php
+++ resources/views/backend/sales/index.blade.php
@@ -18,8 +18,32 @@
             </div><!-- /.container-fluid -->
         </div>
 
-        <div class="container">
-            <button class="btn btn-primary mb-1">Add New</button>
+     <div class="container">
+            <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Add New
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add new sales</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">All Sale</h3>
