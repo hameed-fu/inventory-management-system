4a067472ce24c801f854fcd4ffa9892e5d6bcd48
mustafaviit <mustafakamalawkumcs@gmail.com>
2023-03-16 05:04:38 -0700

    add items

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -68,14 +68,7 @@
                                             <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"
                                                 colspan="1" aria-label="Platform(s): activate to sort column ascending">
                                                 Platform(s)</th>
-                                            <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"
-                                                colspan="1"
-                                                aria-label="Engine version: activate to sort column ascending">Engine
-                                                version</th>
-                                            <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"
-                                                colspan="1" aria-label="CSS grade: activate to sort column ascending">CSS
-                                                grade</th>
-                                        </tr>
+                                           
                                     </thead>
                                     <tbody>
 
@@ -86,79 +79,9 @@
                                             <td>1.7</td>
                                             <td>A</td>
                                         </tr>
-                                        <tr class="even">
-                                            <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
-                                            <td>Firefox 1.5</td>
-                                            <td>Win 98+ / OSX.2+</td>
-                                            <td>1.8</td>
-                                            <td>A</td>
-                                        </tr>
-                                        <tr class="odd">
-                                            <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
-                                            <td>Firefox 2.0</td>
-                                            <td>Win 98+ / OSX.2+</td>
-                                            <td>1.8</td>
-                                            <td>A</td>
-                                        </tr>
-                                        <tr class="even">
-                                            <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
-                                            <td>Firefox 3.0</td>
-                                            <td>Win 2k+ / OSX.3+</td>
-                                            <td>1.9</td>
-                                            <td>A</td>
-                                        </tr>
-                                        <tr class="odd">
-                                            <td class="sorting_1 dtr-control">Gecko</td>
-                                            <td>Camino 1.0</td>
-                                            <td>OSX.2+</td>
-                                            <td>1.8</td>
-                                            <td>A</td>
-                                        </tr>
-                                        <tr class="even">
-                                            <td class="sorting_1 dtr-control">Gecko</td>
-                                            <td>Camino 1.5</td>
-                                            <td>OSX.3+</td>
-                                            <td>1.8</td>
-                                            <td>A</td>
-                                        </tr>
-                                        <tr class="odd">
-                                            <td class="sorting_1 dtr-control">Gecko</td>
-                                            <td>Netscape 7.2</td>
-                                            <td>Win 95+ / Mac OS 8.6-9.2</td>
-                                            <td>1.7</td>
-                                            <td>A</td>
-                                        </tr>
-                                        <tr class="even">
-                                            <td class="sorting_1 dtr-control">Gecko</td>
-                                            <td>Netscape Browser 8</td>
-                                            <td>Win 98SE+</td>
-                                            <td>1.7</td>
-                                            <td>A</td>
-                                        </tr>
-                                        <tr class="odd">
-                                            <td class="sorting_1 dtr-control">Gecko</td>
-                                            <td>Netscape Navigator 9</td>
-                                            <td>Win 98+ / OSX.2+</td>
-                                            <td>1.8</td>
-                                            <td>A</td>
-                                        </tr>
-                                        <tr class="even">
-                                            <td class="sorting_1 dtr-control">Gecko</td>
-                                            <td>Mozilla 1.0</td>
-                                            <td>Win 95+ / OSX.1+</td>
-                                            <td>1</td>
-                                            <td>A</td>
-                                        </tr>
+                                      
                                     </tbody>
-                                    <tfoot>
-                                        <tr>
-                                            <th rowspan="1" colspan="1">Rendering engine</th>
-                                            <th rowspan="1" colspan="1">Browser</th>
-                                            <th rowspan="1" colspan="1">Platform(s)</th>
-                                            <th rowspan="1" colspan="1">Engine version</th>
-                                            <th rowspan="1" colspan="1">CSS grade</th>
-                                        </tr>
-                                    </tfoot>
+                                   
                                 </table>
                             </div>
                         </div>
